a310a2c5f4eb8a34cff15e274dd21c70cdfbc781
dpchurchman <dchurchman@smu.edu>
2018-08-11 14:45:29 -0500

    8/11 draft

--- Participants.R
+++ Participants.R
@@ -9,8 +9,8 @@ dfPartAgeGroup <-  df[c('incident_id','participant_age_group')]
 dfLongTemp <- dfPartAgeGroup %>% separate_rows(2, sep="\\|\\|")
 dfLongTemp <- dfLongTemp[which(dfLongTemp[,2] != ''),]
 
-dfPartAgeGroup <- separate(dfLongTemp,participant_age_group, into=c("part","age_group"), 
-                           sep = "::", remove = TRUE, convert = TRUE)
+dfPartAgeGroup <- suppressWarnings(separate(dfLongTemp,participant_age_group, into=c(
+                    "part","age_group"), sep = "::", remove = TRUE, convert = TRUE))
 
 #Gender
 dfPartGender <-  df[c('incident_id','participant_gender')]
@@ -18,8 +18,8 @@ dfPartGender <-  df[c('incident_id','participant_gender')]
 dfLongTemp <- dfPartGender %>% separate_rows(2, sep="\\|\\|")
 dfLongTemp <- dfLongTemp[which(dfLongTemp[,2] != ''),]
 
-dfPartGender <- separate(dfLongTemp,participant_gender, into=c("part","gender"), 
-                           sep = "::", remove = TRUE, convert = TRUE)
+dfPartGender <- suppressWarnings(separate(dfLongTemp,participant_gender, into=c("part","gender"), 
+                           sep = "::", remove = TRUE, convert = TRUE))
 
 #Relationship
 dfPartRelationship <-  df[c('incident_id','participant_relationship')]
@@ -36,8 +36,8 @@ dfPartStatus <-  df[c('incident_id','participant_status')]
 dfLongTemp <- dfPartStatus %>% separate_rows(2, sep="\\|\\|")
 dfLongTemp <- dfLongTemp[which(dfLongTemp[,2] != ''),]
 
-dfPartStatus <- separate(dfLongTemp,participant_status, into=c("part","status"), 
-                               sep = "::", remove = TRUE, convert = TRUE)
+dfPartStatus <- suppressWarnings(separate(dfLongTemp,participant_status, into=c("part","status"), 
+                               sep = "::", remove = TRUE, convert = TRUE))
 
 #Type: Suspect or Victim
 dfPartType <-  df[c('incident_id','participant_type')]
@@ -45,8 +45,8 @@ dfPartType <-  df[c('incident_id','participant_type')]
 dfLongTemp <- dfPartType %>% separate_rows(2, sep="\\|\\|")
 dfLongTemp <- dfLongTemp[which(dfLongTemp[,2] != ''),]
 
-dfPartType <- separate(dfLongTemp,participant_type, into=c("part","type"), 
-                         sep = "::", remove = TRUE, convert = TRUE)
+dfPartType <- suppressWarnings(separate(dfLongTemp,participant_type, into=c("part","type"), 
+                         sep = "::", remove = TRUE, convert = TRUE))
 
 #Merge all the participant variables
 dfPart <- merge(dfPartType, dfPartStatus, by=c("incident_id","part"), all=TRUE)
@@ -58,10 +58,11 @@ head(dfPart)
 # Get Victims and Suspects Dataframes to Analyze
 dfVictims <- dfPart[which(dfPart$type == "Victim"),]
 dfVictims <- dfVictims[order(dfVictims$incident_id,dfVictims$part),]
+dfVictims <- data.frame(unclass(dfVictims))
+
 
 dfSuspects <- dfPart[which(dfPart$type == "Subject-Suspect"),]
 dfSuspects <- dfSuspects[order(dfSuspects$incident_id,dfSuspects$part),]
-
 dfSuspects <- data.frame(unclass(dfSuspects))
 
 
@@ -70,7 +71,7 @@ suspectTrans <- as(dfSuspects[,4:ncol(dfSuspects)], "transactions")
 suspectTrans
 
 summary(suspectTrans)
-itemFrequencyPlot(suspectTrans, support=0.01, cex.names=0.6)
+itemFrequencyPlot(suspectTrans, support=0.005, cex.names=0.6)
 
 unique(dfSuspects[,4])
 suspectRules <- apriori(suspectTrans,
@@ -88,10 +89,47 @@ suspectRules <- apriori(suspectTrans,
 
 summary(suspectRules)
 
-inspect(head(suspectRules, n=28,by="confidence"))
+inspect(head(suspectRules, n=28,by="lift"))
 library(arulesViz)
-plot(head(suspectRules,n=10, by="lift"), method="graph")#,,engine="htmlwidget",
+plot(head(suspectRules,n=28, by="lift"), method="graph")#,,engine="htmlwidget",
      #igraphLayout = "layout_in_circle")
 
 plot(suspectRules, method = "paracoord", control = list(reorder = TRUE))
+
+plot(suspectRules, method="matrix", measure="lift", control=list(reorder='support/confidence'))
+
+
+
+##########
+###VICTIMS
+library(arules)
+victimTrans <- as(dfVictims[,4:ncol(dfVictims)], "transactions")
+victimTrans
+
+summary(victimTrans)
+itemFrequencyPlot(victimTrans, support=0.005, cex.names=0.6)
+
+unique(dfVictims[,4])
+victimRules <- apriori(victimTrans,
+                        parameter = list(support = 0.01, confidence = 0.01),
+                        appearance = list(rhs=
+                                            c('status=Unharmed, Arrested','status=Unharmed',
+                                              'status=Injured','status=Killed', 
+                                              'status=Injured, Arrested','status=Killed, Unharmed',
+                                              'status=Arrested','status=Killed, Arrested',
+                                              'status=Injured, Unharmed',
+                                              'status=Injured, Unharmed, Arrested',
+                                              'status=Killed, Injured',
+                                              'status=Killed, Unharmed, Arrested'))
+)
+
+summary(victimRules)
+
+inspect(head(victimRules, n=11,by="lift"))
+library(arulesViz)
+plot(head(victimRules,n=9, by="lift"), method="graph")#,,engine="htmlwidget",
+#igraphLayout = "layout_in_circle")
+
+plot(victimRules, method = "paracoord", control = list(reorder = TRUE))
+plot(victimRules)
 
